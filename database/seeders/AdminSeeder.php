7c90f45b20633a32391d7821f5f0661313b86f62
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-09-02 18:22:17 +0700

    make user:seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Nette\Utils\Random;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = \App\Models\User::create([
+            'email' => 'superadmin@admin.com',
+            'password' => Hash::make('superadmin'),
+            'nama' => 'Muhammad Izza Alfiansyah',
+            'role' => '1',
+        ]);
+    }
+}
